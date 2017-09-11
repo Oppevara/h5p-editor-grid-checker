eaee477f93e6e461354670e105f25e21e88edae6
Pjotr Savitski <gnum@tlu.ee>
2017-09-11 21:35:40 +0300

    References Oppevara/oppevara#42, refactoring and docstrings.

--- scripts/grid-checker.js
+++ scripts/grid-checker.js
@@ -1,13 +1,30 @@
 /**
- *
+ * GridChecker editor library
  */
 
 var H5PEditor = H5PEditor || {};
 
 H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
-  // Took the solution from here: https://gist.github.com/jed/982883
+  // Unique identifier code was taked from here: https://gist.github.com/jed/982883
   var generateUuid = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b);};
 
+  var ROWS_AND_COLUMNS_SELECTOR = '.field.group.field-name-rowsAndColumns';
+  var GRID_TABLE_SELECTOR = 'table.h5p-grid-checker.grid';
+  var ROW_SELECTOR = '.field.group.field-name-row';
+  var ROW_ID_SELECTOR = '.field.field-name-rowId > input[type="text"]';
+  var ROW_TEXT_SELECTOR = '.field.field-name-rowText > input[type="text"]';
+  var COLUMN_SELECTOR = '.field.field-name-column';
+  var COLUMN_ID_SELECTOR = '.field.field-name-columnId > input[type="text"]';
+  var COLUMN_TEXT_SELECTOR = '.field.field-name-columnText > input[type="text"]';
+
+  /**
+   * Constructor function
+   * @param       {object} parent     Parent representation
+   * @param       {object} field      Field structure representation
+   * @param       {mixed} params      Array of stored data or undefined
+   * @param       {function} setValue Value storage callback
+   * @constructor
+   */
   function GridChecker(parent, field, params, setValue) {
     this.parent = parent;
     this.field = field;
@@ -15,9 +32,16 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     this.setValue = setValue;
   }
 
+  /**
+   * Generates/regenerates grid table and appends it to container.
+   * Any already present tables are removed.
+   * @return {void}
+   */
   GridChecker.prototype.generateGrid = function() {
-    // TODO This should also reset any old values
-    this.$container.find('table.h5p-grid-checker.grid').remove();
+    this.$container.find(GRID_TABLE_SELECTOR).remove();
+    // This is the last line of defence that should make sure that identifiers
+    // are set before table is generated
+    this.assignUniqueIdentifiers();
 
     var self = this;
     var lookup = self.getLookup();
@@ -32,27 +56,26 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     $('<th>').appendTo(table.find('thead > tr'));
     $.each(columns, function(index, column) {
       $('<th>', {
-        'class': 'h5p-column-' + column.id,
+        'class': 'grid-column-' + column.id,
         'text': column.text
       }).appendTo(table.find('thead > tr'));
     });
     $('<tbody>').appendTo(table);
     $.each(rows, function(rowIndex, row) {
       var tr = $('<tr>', {
-        'class': 'h5p-row-' + row.id
+        'class': 'grid-row-' + row.id
       });
       $('<td>', {
         'text': row.text
       }).appendTo(tr);
       $.each(columns, function(columnIndex, column) {
         var td = $('<td>', {
-          'class': 'h5p-column-' + column.id
+          'class': 'grid-column-' + column.id
         });
         var inputOptions = {
           'name': 'grid-row-' + row.id,
           'value': column.id
         };
-        // TODO Make sure to set the grid values somehow
         if (gridType === 'single') {
           inputOptions.type = 'radio';
         } else if (gridType === 'multiple') {
@@ -74,59 +97,85 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     table.appendTo(this.$responsiveTableContainer);
   };
 
-  GridChecker.prototype.gridTypeChanged = function(e) {
+  /**
+   * Callback for case of grid type being changed
+   * Calls table generation and hides/shows corresponding button
+   * @return {void}
+   */
+  GridChecker.prototype.gridTypeChanged = function() {
     if (this.canGenerateGrid()) {
       this.$generateGridButton.show();
       this.generateGrid();
       return;
     }
 
-    this.$container.find('table.h5p-grid-checker.grid').remove();
+    this.$container.find(GRID_TABLE_SELECTOR).remove();
     this.$generateGridButton.hide();
   };
 
+  /**
+   * Determines if grid could be generated
+   * @return {boolean} Only returns false for textual type
+   */
   GridChecker.prototype.canGenerateGrid = function() {
     var type = this.$gridTypeField.val();
 
     return type === 'single' || type === 'multiple';
   };
 
+  /**
+   * Returns rows data from current DOM representation
+   * @return {array} Array of objects with id and text
+   */
   GridChecker.prototype.getRows = function() {
     var rows = [];
 
-    this.parent.$form.find('.field.group.field-name-row').each(function() {
+    this.parent.$form.find(ROW_SELECTOR).each(function() {
       var element = $(this);
       rows.push({
-        'id': $(this).find('.field.field-name-rowId > input[type="text"]').val(),
-        'text': $(this).find('.field.field-name-rowText > input[type="text"]').val()
+        'id': $(this).find(ROW_ID_SELECTOR).val(),
+        'text': $(this).find(ROW_TEXT_SELECTOR).val()
       });
     });
 
     return rows;
   };
 
+  /**
+   * Returns columns data from current DOM representation
+   * @return {array} Array ob objects with id and text
+   */
   GridChecker.prototype.getColumns = function() {
     var columns = [];
 
-    this.parent.$form.find('.field.field-name-column').each(function() {
+    this.parent.$form.find(COLUMN_SELECTOR).each(function() {
       var element = $(this);
       columns.push({
-        'id': $(this).find('.field.field-name-columnId > input[type="text"]').val(),
-        'text': $(this).find('.field.field-name-columnText > input[type="text"]').val()
+        'id': $(this).find(COLUMN_ID_SELECTOR).val(),
+        'text': $(this).find(COLUMN_TEXT_SELECTOR).val()
       });
     });
 
     return columns;
   };
 
+  /**
+   * Set unique identifiers to any rows or columns that are still missing those
+   * Uses UUID V4 like identifiers
+   * @return {void}
+   */
   GridChecker.prototype.assignUniqueIdentifiers = function() {
-    this.parent.$form.find('.field.group.field-name-rowsAndColumns').find('.field.field-name-rowId > input, .field.field-name-columnId > input').filter(function() {
+    this.parent.$form.find(ROWS_AND_COLUMNS_SELECTOR).find(ROW_ID_SELECTOR + ', ' + COLUMN_ID_SELECTOR).filter(function() {
       return !this.value;
     }).each(function() {
       $(this).val(generateUuid()).trigger('change');
     });
   };
 
+  /**
+   * Returns a lookup table based on either current values or previously stored ones
+   * @return {object} Keyed by row identifiers with arrays of columnsidentifiers
+   */
   GridChecker.prototype.getLookup = function() {
     var params = this.storedParams ? this.storedParams : this.params;
     var lookup = {};
@@ -138,8 +187,13 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     return lookup;
   };
 
+  /**
+   * Determines current checked grid values and stores those
+   * A local temporary copy is also kept for quick reference
+   * @return {boolean}
+   */
   GridChecker.prototype.save = function() {
-    var table = this.$container.find('table.h5p-grid-checker.grid').get(0);
+    var table = this.$container.find(GRID_TABLE_SELECTOR).get(0);
 
     if (table) {
       var rows = this.getRows();
@@ -163,6 +217,12 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     return false;
   };
 
+  /**
+   * Builds the DOM objects and appends to the $wrapper
+   * Also deals with setup of listeners and event handlers
+   * @param  {object} $wrapper DOM node of contsainer element
+   * @return {void}
+   */
   GridChecker.prototype.appendTo = function($wrapper) {
     var self = this;
     var baseDataChangedHandler = function() {
@@ -175,6 +235,7 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
       'class': 'field field-name-' + self.field.name + ' h5p-grid-checker group'
     });
 
+    // XXX This should probably ge selected with H5P own methods
     self.$gridTypeField = self.parent.$form.find('.field.field-name-' + self.field.gridChecker.typeField + '.select > select');
 
     self.$generateGridButton = $('<div>', {
@@ -191,6 +252,7 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     if (!self.canGenerateGrid()) {
       self.$generateGridButton.hide();
     }
+    // XXX This could be done with H5P own methods
     self.$gridTypeField.on('change', function() {
       self.gridTypeChanged();
     });
@@ -204,22 +266,32 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     // Generate identifiers for fields with empty value
     self.assignUniqueIdentifiers();
     // Setup listeners to regerate identifiers as fields get added
-    self.parent.$form.find('.field.group.field-name-rowsAndColumns .h5peditor-button.h5peditor-button-textual').on('click', function() {
+    self.parent.$form.find(ROWS_AND_COLUMNS_SELECTOR + ' .h5peditor-button.h5peditor-button-textual').on('click', function() {
       self.assignUniqueIdentifiers();
-      self.parent.$form.find('.field.group.field-name-rowsAndColumns .h5peditor-button').off('click', baseDataChangedHandler);
-      self.parent.$form.find('.field.group.field-name-rowsAndColumns .h5peditor-button').on('click', baseDataChangedHandler);
-      self.parent.$form.find('.field.group.field-name-row .field.field-name-rowText > input[type="text"], .field.group.field-name-column .field.field-name-columnText > input[type="text"]').off('change', baseDataChangedHandler);
-      self.parent.$form.find('.field.group.field-name-row .field.field-name-rowText > input[type="text"], .field.group.field-name-column .field.field-name-columnText > input[type="text"]').on('change', baseDataChangedHandler);
+      self.parent.$form.find(ROWS_AND_COLUMNS_SELECTOR + ' .h5peditor-button')
+        .off('click', baseDataChangedHandler)
+        .on('click', baseDataChangedHandler);
+      self.parent.$form.find(ROW_SELECTOR + ' ' + ROW_TEXT_SELECTOR + ', ' + COLUMN_SELECTOR + ' ' + COLUMN_TEXT_SELECTOR)
+        .off('change', baseDataChangedHandler)
+        .on('change', baseDataChangedHandler);
     });
 
-    self.parent.$form.find('.field.group.field-name-rowsAndColumns .h5peditor-button').on('click', baseDataChangedHandler);
-    self.parent.$form.find('.field.group.field-name-row .field.field-name-rowText > input[type="text"], .field.group.field-name-column .field.field-name-columnText > input[type="text"]').on('change', baseDataChangedHandler);
+    self.parent.$form.find(ROWS_AND_COLUMNS_SELECTOR + ' .h5peditor-button')
+      .on('click', baseDataChangedHandler);
+    self.parent.$form.find(ROW_SELECTOR + ' ' + ROW_TEXT_SELECTOR + ', ' + COLUMN_SELECTOR + ' ' + COLUMN_TEXT_SELECTOR)
+      .on('change', baseDataChangedHandler);
 
     if (self.params && self.canGenerateGrid) {
       self.generateGrid();
     }
   };
 
+  /**
+   * Runs before page is saved, makes sure the values are stored.
+   * Sets value to undefined, if data table is missing.
+   * Does not really do any validation.
+   * @return {boolean} Always returns true
+   */
   GridChecker.prototype.validate = function() {
     if (this.save()) {
       return true;
@@ -229,10 +301,15 @@ H5PEditor.widgets.gridChecker = H5PEditor.GridChecker = (function($) {
     return true;
   };
 
+  /**
+   * Handles element removal
+   * @return {void}
+   */
   GridChecker.prototype.remove = function() {
     $wrapper.remove();
   };
 
+  // Additional library specific translation stings
   H5PEditor.language['H5PEditor.GridChecker'] = {
     'libraryStrings': {
       'generateGrid': 'Click to generate grid and mark correct options'
